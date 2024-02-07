ddcb804a542a490b87f9f5e9a2ba8aa596ec6a8f
Marcos Gabriel <marcosgabriel79355@gmail.com>
2024-02-07 10:16:58 -0300

    Tratando exceção para obejeto não encontrado

--- src/main/java/br/com/helpdesk/services/TecnicoService.java
+++ src/main/java/br/com/helpdesk/services/TecnicoService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import br.com.helpdesk.domain.Tecnico;
 import br.com.helpdesk.repositories.TecnicoRepository;
+import br.com.helpdesk.services.exceptions.ObjectnotFoundException;
 
 @Service
 public class TecnicoService {
@@ -16,6 +17,6 @@ public class TecnicoService {
 	
 	public Tecnico findByid(Integer id) {
 		Optional<Tecnico> obj = repository.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(()-> new ObjectnotFoundException("Objeto não encontrado! id: " +id));
 	}
 }
