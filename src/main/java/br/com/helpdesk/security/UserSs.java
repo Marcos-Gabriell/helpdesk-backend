a8f1cc1e71df288de51ad0e620b9cbfab984e62e
Marcos Gabriel <marcosgabriel79355@gmail.com>
2024-02-10 17:23:23 -0300

    Ajuste

--- src/main/java/br/com/helpdesk/security/UserSs.java
+++ src/main/java/br/com/helpdesk/security/UserSs.java
@@ -11,6 +11,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import br.com.helpdesk.domain.enums.Perfil;
 
 public class UserSs implements UserDetails{
+	private static final long serialVersionUID = 1L;
 	
 	private Integer id;
 	private String email;
